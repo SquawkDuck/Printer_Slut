37a2748d643ac5904abe576e6987cf8ace0ff5ff
SquawkDuck <fabrizio.giorgi1982@gmail.com>
2025-08-25 16:17:20 +0800

    broken
    Dario dont look at this I'm working on some changes

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
-{ // ora non inquino piu' :)
+function CylingMessage() {
   const frasi = ["Dario", "saro'", "la troia", "informatica", "della ditta"];
   let index = 0;
-
-  setInterval(function () {
-
-    document.getElementById("screen_message").textContent = frasi[index];
-    index = (index + 1) % frasi.length; // Visto come ho usato l'operatore modulo invece di if-else ? 
-  }, 800);
 }
+setInterval(CylingMessage, 800); {
+
+  document.getElementById("screen_message").textContent = frasi[index];
+  index = (index + 1) % frasi.length; // Visto come ho usato l'operatore modulo invece di if-else ? 
+}, 800);
+
+const updateScreenMessage = () => {
+  document.getElementById("screen_message").textContent = messages[currentIndex];
+  currentIndex = (currentIndex + 1) % messages.length;
+};
+
+setInterval(updateScreenMessage, 2000);
