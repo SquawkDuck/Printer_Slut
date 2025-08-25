bbf762156f213a02987c4072ab3819085c950883
SquawkDuck <fabrizio.giorgi1982@gmail.com>
2025-08-25 18:50:56 +0800

    working
     almost done just work on the first h1 value

--- test.js
+++ test.js
@@ -1,16 +1,10 @@
-function CylingMessage() {
-  const frasi = ["Dario", "saro'", "la troia", "informatica", "della ditta"];
+function Main() {
   let index = 0;
+  const frasi = ["Dario", "saro'", "la troia", "informatica", "della ditta"];
+  function CylingMessage() {
+    document.getElementById("screen_message").textContent = frasi[index];
+    index = (index + 1) % frasi.length;
+  }
+  setInterval(CylingMessage, 800);
 }
-setInterval(CylingMessage, 800); {
-
-  document.getElementById("screen_message").textContent = frasi[index];
-  index = (index + 1) % frasi.length; // Visto come ho usato l'operatore modulo invece di if-else ? 
-}, 800);
-
-const updateScreenMessage = () => {
-  document.getElementById("screen_message").textContent = messages[currentIndex];
-  currentIndex = (currentIndex + 1) % messages.length;
-};
-
-setInterval(updateScreenMessage, 2000);
+Main();
