d09fd280e67eec40a8b252001a57ee7f3f8069c1
SquawkDuck <fabrizio.giorgi1982@gmail.com>
2025-08-26 10:24:29 +0800

    the new shit

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 function Main() {
   let index = 0;
   const frasi = ["Dario", "saro'", "la troia", "informatica", "della ditta"];
-  function CylingMessage() {
+  function cylingMessage() {
     document.getElementById("screen_message").textContent = frasi[index];
     index = (index + 1) % frasi.length;
   }
-  setInterval(CylingMessage, 800);
+  setInterval(cylingMessage, 800);
 }
 Main();
