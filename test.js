02bbc1a6b4b83b924eb93a08b4148876cda10ebe
SquawkDuck <fabrizio.giorgi1982@gmail.com>
2025-08-29 15:01:12 +0800

    updated

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
-function generateStupidMessage() {
+function main() {
   let index = 0;
   const frasi = ["Dario", "saro'", "la troia", "informatica", "della ditta"];
+  const screenMessage = document.getElementById("screen_message");
+  const buttonMesage = document.getElementById("button_message");
+  console.log(buttonMesage);
+  let paused = false;
+  const intervalLenght = 800;
+  let interval = setInterval(cycleMessage, intervalLenght);
+  function pauseUnpause() {
+    paused = !paused;
+    if (paused) {
+      clearInterval(interval);
+      buttonMessage.textContent = "unpause";
+      console.log("paused");
+    } else {
+      interval = setInterval(cycleMessage, intervalLenght);
+      buttonMessage.textContent = "pause";
+      console.log("unpaused");
+    }
+  }
+  buttonMesage.addEventListener("click", pauseUnpause);
   function cycleMessage() {
-    document.getElementById("screen_message").textContent = frasi[index];
+    screenMessage.textContent = frasi[index];
     index = (index + 1) % frasi.length;
-  };
-  cycleMessage();
-  setInterval(cycleMessage, 800);
+  }
 }
-generateStupidMessage();
+main();
