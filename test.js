9235f672b1cf9dc8dd4f398a61fb02cfd66e24dd
SquawkDuck <fabrizio.giorgi1982@gmail.com>
2025-08-29 11:47:39 +0800

    xx

--- test.js
+++ test.js
@@ -1,42 +1,11 @@
 function generateStupidMessage() {
-  document.getElementById("button_message").style.backgroundColor = "red";
-  const ButtonMessage = "Stop if you had enought !"
-  document.getElementById("button_message").textContent = ButtonMessage;
   let index = 0;
-
-  async function getJokes() {
-  try {
-    const response = await fetch('jokes.txt');
-    const text = await response.text();
-    const jokes = text.split('\n');
-    return jokes;
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-  let paused = false;
-  async function cycleMessage() {
-   if (!paused) {
-     const jokes = await getJokes();
-      document.getElementById("screen_message").textContent = jokes[index];
-      index = (index + 1) % jokes.length;
-    }
-  }
-  const intervalLenght = 3500;
-  cycleMessage();
-  let intervalId = setInterval(cycleMessage, intervalLenght);
-  stopStartInterval = function () {
-    if (paused) {
-      clearInterval(intervalId);
-      document.getElementById("button_message").textContent = ButtonMessage;
-      document.getElementById("button_message").style.backgroundColor = "red";
-    } else {
-      intervalId = setInterval(cycleMessage, intervalLenght);
-      document.getElementById("button_message").textContent = "Keep going, I actually like it !";
-      document.getElementById("button_message").style.backgroundColor = "green";
-    }
-    paused = !paused;
+  const frasi = ["Dario", "saro'", "la troia", "informatica", "della ditta"];
+  function cycleMessage() {
+    document.getElementById("screen_message").textContent = frasi[index];
+    index = (index + 1) % frasi.length;
   };
+  cycleMessage();
+  setInterval(cycleMessage, 800);
 }
 generateStupidMessage();
