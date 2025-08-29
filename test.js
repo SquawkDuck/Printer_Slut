949ad07b61259494a8c93945027afc087d0234c0
SquawkDuck <fabrizio.giorgi1982@gmail.com>
2025-08-29 21:33:50 +0800

    trying to get the .txt file

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 function main() {
   let index = 0;
+  function getTextFile(filename) {// this function is to fetch the file 
+    fetch(filename)
+      .then((response) => response.text())
+      .then((data) => {
+        const array = data.split("\n");
+        console.log(array);
+      });
+  }
+ // getTextFile(jokes.txt); can't find it in the context of main()
   const frasi = ["Dario", "saro'", "la troia", "informatica", "della ditta"];
   const screenMessage = document.getElementById("screen_message");
   const buttonMessage = document.getElementById("button_message");
